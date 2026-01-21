eafc39842376ea694456f150ab869f7ad3709400
peter giampaoli <petergiampaoli@gmail.com>
2026-01-21 18:32:46 -0500

    formatting error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
+    <React.StrictMode>
+       <App />
+    </React.StrictMode>
+);

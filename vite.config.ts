689fd09cbd491108a877e9debb133f321ad83c40
peter giampaoli <petergiampaoli@gmail.com>
2026-01-21 18:41:37 -0500

    spelling error

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:"/sad-skol"
+  base:"./sad-skol",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
